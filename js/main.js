21943135f0ca14f9b5b2264902f13406964e3a6e
narayanagung <agungnarayanagung@gmail.com>
2023-10-29 21:14:15 +0800

    1.0 published

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-const audio = document.getElementById("audio");
-
 audio.addEventListener("timeupdate", (e) => {
-	const currentTime = audio.currentTime;
+	const audio = document.getElementById("audio");
 	const lyrics = document.getElementById("lyrics");
+
+	const currentTime = audio.currentTime;
 
 	for (let i = 0; i < lyrics.children.length; i++) {
 		const line = lyrics.children[i];
@@ -10,21 +10,46 @@ audio.addEventListener("timeupdate", (e) => {
 		const startTime = parseFloat(line.getAttribute("timestamp"));
 		const endTime = nextLine ? parseFloat(nextLine.getAttribute("timestamp")) : audio.duration;
 
+		//When the lyrics timestamp is matching the audio time : highlight the lyric
 		if (currentTime >= startTime && currentTime < endTime) {
 			line.classList.add("active");
-			line.scrollIntoView({
-				behavior: "smooth",
-				block: "center",
-			});
+			document.getElementById("overflow").style.overflowY = "scroll";
+
+			//Auto scroll the highlighted lyric if toggle button is "ON"
+			if (autoScroll) {
+				document.getElementById("overflow").style.overflowY = "hidden";
+				line.scrollIntoView({
+					behavior: "smooth",
+					block: "center",
+				});
+			}
 		} else {
 			line.classList.remove("active");
 		}
 	}
 });
 
+//Auto scroll toggle button default "ON"
+let autoScroll = true;
+
+toggleAutoScroll.addEventListener("click", () => {
+	const toggleAutoScroll = document.getElementById("toggleAutoScroll");
+	autoScroll = !autoScroll;
+	toggleAutoScroll.textContent = autoScroll ? "🔒 Auto Scroll ON" : "🔓 Auto Scroll OFF";
+});
+
+//Clicking any lyrics will make the audio jump to their timestamp
 lyrics.addEventListener("click", (e) => {
 	if (e.target.tagName === "P") {
 		const startTime = parseFloat(e.target.getAttribute("timestamp"));
 		audio.currentTime = startTime;
+	}
+});
+
+// Prevent the default behavior of space button & set the focus to media control
+document.addEventListener("keydown", function (e) {
+	if (e.key === " ") {
+		e.preventDefault();
+		audio.focus();
 	}
 });
